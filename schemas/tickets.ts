ff2282a5a368c488df0d670cc19dda918157d87f
Benjamin <benjamin@libeo.io>
2020-02-11 20:43:30 +0100

    refactor: use typescript

--- schemas/tickets.ts
+++ schemas/tickets.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema,
   ticketSchema = new Schema({
-    tips: Number, // Pourboire laissé
-    amount: Number // Montant du billet
+    tips: Number,
+    amount: Number
   }),
   Ticket =
     mongoose.modelNames().indexOf("Ticket") == -1
