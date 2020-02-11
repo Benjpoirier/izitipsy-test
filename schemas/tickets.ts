dc2dfb995c012ce4624831ba34a8cac5e8f0ad7f
Benjamin <benjamin@libeo.io>
2020-02-11 22:13:44 +0100

    refactor(ticket schema): ticket is now typesafe

--- schemas/tickets.ts
+++ schemas/tickets.ts
@@ -1,13 +1,13 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
+export interface Ticket extends Document {
+  tips: number,
+  amount: number
+}
 
-const Schema = mongoose.Schema,
-  ticketSchema = new Schema({
+const Schema = mongoose.Schema;
+const ticketSchema = new Schema({
     tips: Number,
     amount: Number
-  }),
-  Ticket =
-    mongoose.modelNames().indexOf("Ticket") == -1
-      ? mongoose.model("Ticket", ticketSchema)
-      : mongoose.model("Ticket");
+  });
 
-export default Ticket;
+export default mongoose.modelNames().includes('tickets') ? mongoose.model<Ticket>("Ticket", ticketSchema) : mongoose.model<Ticket>("Ticket");;
