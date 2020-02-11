d5dfc52be26ce64a0afd579ead51b4af1bdc53ac
Benjamin <benjamin@libeo.io>
2020-02-11 22:14:32 +0100

    feat(dashboard): fetch tickets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import { NextPage } from "next"
+import { Ticket } from "../schemas/tickets"
 
-const Dashboard: NextPage = () => {
+const Dashboard: NextPage<{tickets: Ticket[]}> = ({tickets}) => {
     return <>Dashboard</>
+}
+
+Dashboard.getInitialProps = async () => {
+    const response = await fetch('http://localhost:3000/api/tickets')
+    const tickets = await response.json()
+    return { tickets }
 }
 
 export default Dashboard
