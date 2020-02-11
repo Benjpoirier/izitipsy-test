526cb6792956d0c78e95b80feedfc2ffa32c5166
Benjamin <benjamin@libeo.io>
2020-02-11 21:01:17 +0100

    refactor(db): add type

--- utils/db.ts
+++ utils/db.ts
@@ -1,16 +1,22 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose"
+import { NextApiResponse, NextApiRequest } from "next";
 
-export default handler => async (req, res) => {
+type Handler = (request: NextApiRequest, response: NextApiResponse) => Promise<void>
+
+export default (handler: Handler): Handler => async (req, res) => {
   const db = mongoose.connection;
 
   console.log("db.readyState", db.readyState);
 
-  if (db.readyState == 1) return handler(req, res);
+  if (db.readyState == 1) {
+    return handler(req, res);
+  }
 
   mongoose.connect("credentialsToAdd", { useNewUrlParser: true });
 
   db.on("error", console.error.bind(console, "connection error:"));
-  db.once("open", function() {
+
+  db.once("open", () => {
     console.log("We are connected to the DB");
     return handler(req, res);
   });
